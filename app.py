eb795e1af498929bb49cd9561aaf59f9c06c2e4a
Gael Grezz <gaelgutierrezdev@gmail.com>
2025-05-27 21:58:20 -0500

    fix, feature: Soluciona errores de lógica y se agrega mensajes de confirmación

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import pandas as pd
 import streamlit as st
 import io
 
-# Estado inicial
 if "archivo_disponible" not in st.session_state:
     st.session_state["archivo_disponible"] = False
 
@@ -10,29 +9,24 @@ if "archivo_consolidado" not in st.session_state:
     st.session_state["archivo_consolidado"] = None
 
 
-def saveValue():
-    st.session_state["archivo_disponible"] = True
-
-
-def convertirExcel(archivo, nombre_archivo="archivo"):
+@st.cache_data
+def convertirExcel(archivo):
     output = io.BytesIO()
     archivo.to_excel(output, index=True)
-    output.seek(0)  # Mueve el cursor al inicio del archivo
-    return output
+    output.seek(0)
+    return output  # Eliminamos los globos aquí
 
 
-def consolidarArchivo(archivo, nombre_archivo="Consolidado"):
-    saveValue()
-    st.text("Consolidando datos... ⏳")
-    
+@st.cache_data
+def consolidarArchivo(archivo):
     df = pd.read_excel(archivo)
     Encabezados = list(df.columns)
-    
+
     df[Encabezados[0]] = df[Encabezados[0]].astype(str).str.slice(0, 16)
     df = df.groupby(Encabezados[0]).mean()
 
-    st.session_state["archivo_consolidado"] = df
-    st.balloons()
+    st.balloons() 
+    return df
 
 
 col1, col2 = st.columns([1, 2], gap="medium", vertical_alignment="center")
@@ -45,20 +39,20 @@ with col2:
         "✨ Haz clic o arrastra tu archivo aquí para comenzar 🚀",
         ["csv", "xlsx"],
         accept_multiple_files=False,
-        help='Sube tu archivo, posteriormente se despliega el botón "consolidar" para continuar con el proceso.'
+        help='Sube tu archivo, posteriormente se despliega el botón "Consolidar" para continuar con el proceso.',
     )
 
     if archivo:
         st.caption("Haz clic sobre ✖️ para eliminar el archivo.")
-    
-        consolidar = st.button(
+
+        if st.button(
             "Consolidar :material/sync:",
-            on_click=consolidarArchivo,
             use_container_width=True,
             help="Haz clic para empezar a consolidar tu archivo.",
             key="consolidar",
-            args=(archivo,)
-        )
+        ):
+            st.session_state["archivo_consolidado"] = consolidarArchivo(archivo)
+
     else:
         st.caption(
             "<b>Recuerda que:</b> <br/> - Solo puedes seleccionar un único archivo 📄 para este proceso. <br/> - Admite CSV y XLSX hasta 30MB.",
@@ -67,15 +61,22 @@ with col2:
 
 st.divider()
 
-if "archivo_consolidado" in st.session_state and st.session_state["archivo_consolidado"] is not None:
-    archivo = st.session_state["archivo_consolidado"]
-    archivo_Excel = convertirExcel(archivo)
-    
+if (
+    "archivo_consolidado" in st.session_state
+    and st.session_state["archivo_consolidado"] is not None
+):
+    st.success("Consolidación hecha con éxito ✅")
+    archivo_Excel = convertirExcel(st.session_state["archivo_consolidado"])
+
     st.download_button(
         label="📥 Descargar archivo consolidado",
         data=archivo_Excel,
         file_name="Consolidados.xlsx",
-        mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+        mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+        key="descargar",
     )
+
 else:
-    st.text("Aquí se mostrará tu archivo 📄 una vez se haya concluido con la consolidación de datos.")
+    st.text(
+        "Aquí se mostrará tu archivo 📄 una vez se haya concluido con la consolidación de datos."
+    )
